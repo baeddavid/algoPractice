57e6b474849b895a194a2ec972574d2536a0ad4b
David Bae <baeddavid@gmail.com>
2019-08-20 22:53:11 -0500

    add comments to maxDepth

--- LeetCode/maxDepthBST.java
+++ LeetCode/maxDepthBST.java
@@ -8,8 +8,12 @@
  * }
  */
 class Solution {
+    // Recursively check the depth of the tree
     public int maxDepth(TreeNode root) {
+        // If the root is null return 0
         if(root == null) return 0;
+        // Else take the max of either the left subtree + 1 and right subtree + 1
+        // and return it
         return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
     }
 }
