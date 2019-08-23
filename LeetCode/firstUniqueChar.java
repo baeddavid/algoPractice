317e718d021b4482c1680f040ba0754e9497530a
David Bae <baeddavid@gmail.com>
2019-08-23 07:47:35 -0500

    update description

--- LeetCode/firstUniqueChar.java
+++ LeetCode/firstUniqueChar.java
@@ -3,7 +3,9 @@ import java.util.HashMap;
 class Solution {
     // Hashmap Solution
     // Time: O(n)
-    // Space: O(1)
+    // Space: O(1) -> Although we use map, the problem states there are only 
+    // lower cases in the string. This means we can guarantee that there is always
+    // a maximum space of 26.
     public int firstUniqChar(String s) {
         HashMap<Character, Integer> map = new HashMap();
         for(int i = 0; i < s.length(); i++) 
