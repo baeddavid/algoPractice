ec4fcba0d5364a8d8ed84499435c2f2f5363639b
David Bee <baeddavid@gmail.com>
2019-12-03 19:28:30 -0600

    update ways to solve cutting rod problem

--- CLRS/Algorithms/RodCutting.java
+++ CLRS/Algorithms/RodCutting.java
@@ -16,4 +16,35 @@ public class RodCutting {
         }
         return val[length];
     }
+
+    public int cutRod(int[] prices, int length) {
+        if(length == 0) return 0;
+        int a = Integer.MIN_VALUE;
+        for(int i = 0; i < length; i++)
+            a = max(a, prices[i] + cutRod(prices, length - 1));
+        return a;
+    }
+
+    public int memoizedCutRod(int[] prices, int length) {
+        int[] r = new int[length + 1];
+        for(int i = 0; i <= length; i++)
+            r[i] = Integer.MIN_VALUE;
+        return memoizedCutRodAux(prices, length, r);
+    }
+
+    private int memoizedCutRodAux(int[] p, int length, int[] r) {
+        int a;
+        if(r[length] >= 0)
+            return r[length];
+        if(length == 0)
+            a = 0;
+        else {
+            a = Integer.MIN_VALUE;
+            for(int i  = 0; i < length; i++) {
+                a = max(a, p[i] + memoizedCutRodAux(p, length - i, r));
+            }
+        }
+        r[length] = a;
+        return a;
+    }
 }
